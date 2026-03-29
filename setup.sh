87634572f112ddd771138a57328919abf3dbccf3
Mhawar <abderrahmanemhawar@gmail.com>
2026-03-29 12:53:18 +0100

    v1.0.1: sync: update configs from .config and add .bashrc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Setting up dotfiles..."
 
+# Bash
+ln -sf "$DOTFILES_DIR/bash/rc" ~/.bashrc
+
 # Hyprland
 mkdir -p ~/.config/hypr/scripts
 ln -sf "$DOTFILES_DIR/hypr/scripts/start-remote-displays.sh" ~/.config/hypr/scripts/
